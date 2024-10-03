b70fa657d8ae4947c73587c774609279cb163216
楊玗真 <rtly790813@gmail.com>
2024-10-03 15:18:20 +0800

    feat: uncommend route file

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
 /** @format */
 
 import React, { PropsWithChildren } from "react";
+import SideNav from "@/app/ui/dashboard/sidenav";
 
 const Layout = ({ children }: PropsWithChildren) => {
     return (
-        <div>
-            <p>Dashbard Layout</p>
-            {children}
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+                <SideNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,31 +12,7 @@ import NavLinks from "./ui/dashboard/nav-links";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${inter.variable}  antialiased`}>
-                <main className="flex min-h-screen flex-col p-6">
-                    <div
-                        className={` flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52`}>
-                        <AcmeLogo />
-                        <div className={styles.shape} />
-                        <NavLinks />
-                        <ul>
-                            {/* <li>
-                                <a href="/">Home</a>
-                            </li>
-                            <li>
-                                <a href="/about">About</a>
-                            </li> */}
-                            {/* <li>
-                                <Link href="/">Home</Link>
-                            </li> */}
-                            {/* <li>
-                                <Link href="/about">About</Link>
-                            </li> */}
-                        </ul>
-                    </div>
-                    <div className="mt-4 flex grow flex-col gap-4 md:flex-row">{children}</div>
-                </main>
-            </body>
+            <body className={`${inter.className} antialiased`}>{children}</body>
         </html>
     );
 }
