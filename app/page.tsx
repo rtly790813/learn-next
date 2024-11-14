96c1ef170ae4e2c682e7eb5e70c07d4cef274224
楊玗真 <rtly790813@gmail.com>
2024-11-14 09:55:51 +0800

    feat: change position

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ export default function Page() {
         <>
             <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
                 <AcmeLogo />
-                <LiffComponent />
             </div>
+            <LiffComponent />
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
                     {/* 測試不同 css module 編譯後的 class name 會不一樣，都是 {檔案名稱開頭}_{className}_{random} */}
